0b630bd598fa0d2fad85162662b1d9322ca02eff
hopegipson <gipson.hope@gmail.com>
2021-05-03 22:15:31 -0600

    Changing nav

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -16,7 +16,7 @@ class NavBar extends Component {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#features">About</Nav.Link>
                 <Nav.Link href="#pricing">Pricing</Nav.Link>
                 <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
