9aada7791a30f5458a5fed94e0506ca081837036
hopegipson <gipson.hope@gmail.com>
2021-05-12 23:58:13 -0600

    Created reviews container

---
+++ frontend/src/containers/ReviewsContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+export default function reviewsContainer() {	
+	const [reviews, setReviews] = useState([])
+
+	useEffect(()=> {
+		fetch('')
+			.then(response => response.json())
+			.then(res => setState(res.data))
+		}
+	, [])
+
+	return (
+		<div>
+
+		<ul>
+		{reviews.map(review => (
+			<li key={review.id}>
+			<h2> {review.text} </h2>
+			<h3> {review.author} </h3>
+			</li>
+		))}
+		</ul>
+	
+		</div>
+	)
+
+}
