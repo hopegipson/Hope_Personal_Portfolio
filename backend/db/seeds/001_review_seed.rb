531e9791961834c4e526ea7d9ccc98a2b84be438
hopegipson <gipson.hope@gmail.com>
2021-05-21 23:18:48 -0600

    Finished seeds file

--- backend/db/seeds/001_review_seed.rb
+++ backend/db/seeds/001_review_seed.rb
@@ -6,3 +6,18 @@ DATA = {
       ["ninoalejandro", "This was an amazing experience. Such good communication and super fast delivery. Can't wait to work with you again on more projects. I was sent more than one version so I could choose what I wanted. Thanks again.", 5, "Phillippines"]
     ]}
   
+    def main
+      make_reviews
+    end
+
+    def make_reviews
+      DATA[:reviews].each do |review|
+        new_review = Review.new
+        review.each_with_index do |attribute, i|
+          new_review.send(DATA[:review_keys][i]+"=", attribute)
+        end
+        new_review.save
+      end
+    end
+
+    main
