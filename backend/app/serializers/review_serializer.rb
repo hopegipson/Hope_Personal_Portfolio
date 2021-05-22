49882a2022c9b7a25c53e2adda36b29babfd67ba
hopegipson <gipson.hope@gmail.com>
2021-05-21 23:01:54 -0600

    Serializer of reviews

---
+++ backend/app/serializers/review_serializer.rb
@@ -0,0 +1,9 @@
+class ReviewSerializer < ActiveModel::Serializer
+    def initialize(review_object)
+      @review = review_object
+    end
+   
+    def to_serialized_json
+      @review.to_json(include: [:author, :text, :rating], :except => [:updated_at, :created_at])
+    end
+  end
