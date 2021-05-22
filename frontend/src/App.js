a326135e2ccc75f47c6c05513fb3bba03cca9c99
hopegipson <gipson.hope@gmail.com>
2021-05-21 22:38:52 -0600

    Cleaned up App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
 import NavBar1 from './components/NavBar'
-import YoutubeEmbed from './components/YoutubeEmbed';
-import YoutubeDescription from './components/YoutubeDescription';
-import GigsContainer from './containers/GigsContainer';
 import HomePage from './containers/HomePage';
-
-
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AboutUs from './containers/AboutUs';
 
