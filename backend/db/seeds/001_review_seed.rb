9e58ed7577e01bbdfa878d3d1adae2a626c203aa
hopegipson <gipson.hope@gmail.com>
2021-05-21 23:47:46 -0600

    Added reviews to seeds

--- backend/db/seeds/001_review_seed.rb
+++ backend/db/seeds/001_review_seed.rb
@@ -1,3 +1,5 @@
+#scrape a site with a load more
+
 DATA = {
     :review_keys =>
       ["author", "content", "rating", "location"],
@@ -8,6 +10,15 @@ DATA = {
       ["kohop91", "Incredible. I will absolutely be back for more masters. My expectations were blown out of the water, obliterated. Such sincere & professional communication/consideration with such IMPRESSIVE turnaround time is so rare anymore, with anything, let alone your most personal, unpolished artistic endeavors. First time ever using Fiverr, and I gotta say... I'm blown away. If you are on the fence or just browsing out of curiosity, take a leap of faith with Nerdmilkstudios.", 5, "United States"],
       ["kingsofconfetti", "Everything was exactly as I asked for it. Super fast and excellent quality!", 5, "United States"],
       ["jauntae_smith", "I was what you expect fast quick and easy", 5, "United States"],
+      ["cindyrella713", "It is such a pleasure to work with this seller, I highly recommend him. Extremely professional and friendly, one of the top sellers on Fiverr.", 5, "United States"],
+      ["mel0908", "One of the best mastering service here in Fiver. Her priority is customers satisfaction, she's always willing to amend the track until you are satisfied. She's really good in this job, very friendly, professional and always deliver a quality product and on time. I highly recommended her.", 5, "United Kingdom"],
+      ["aaronss23", "Finished product was okay but the vocals had a static toward it whereas in the past I paid less but the work seemed to be of greater quality. Also the work given was cleaned and prepped to the fullest unlike the past work I had sent to them. Great communication but I really am not sure what went wrong compared previous work, maybe they had a lot of work to where they couldn't get mine done in a timely manner. I understand.", 3.7, "United States"],
+      ["kevinye94", "Very friendly and receptive seller. Had plans to master it myself but glad I took it to her instead. Awesome mastering. Will be back!", 5, "United Kingdom"],
+      ["ataraxisofficia", "Great experience but there was some issues with the order at first but she was patient and in the end she was able to produce the sound that I wanted", 5, "United States"],
+      ["garveydutes", "went above and beyond any expectations!", 5, "United States"],
+      ["stardiskstudios", "I had a loooot of requests and she was more than responsive to all of them. She was even more helpful than I could have HOPED for! I would definitely recommend her as her work is quite good as is her character!", 5, "Japan"],
+      ["daviska123", "Wow, Absolutely amazing, it is perfect just how I imagined it in my head, definitely gonna buy this again!!!", 5, "United States"],
+      ["alysco2003", "Wonderful to work with and gave me exaywhat I asked for!! Will be back!", 5, "United States"],
     ]}
   
     def main
