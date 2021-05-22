4cf7b3f14a2174fd6bcadc670ae40e7bce498ee4
hopegipson <gipson.hope@gmail.com>
2021-05-21 23:06:31 -0600

    Added controller actions to reviews controller

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+
+    def create
+        review = Review.create(author: review_params[:author], content: review_params[:content], rating: review_params[:rating])
+        render json: ReviewSerializer.new(review).to_serialized_json
+      end
+
+    def index
+        reviews = Review.all
+        render json: ReviewSerializer.new(reviews).to_serialized_json
+      end
+
+      def show
+        review = Review.find(params[:id])
+        render json: ReviewSerializer.new(review).to_serialized_json
+      end
+
+      private
+
+      def review_params
+        params.require(:review_info).permit(:author, :content, :rating)
+      end
 end
