020165b8b8519a51e8db7d5734a8f01c4544d9e8
hopegipson <gipson.hope@gmail.com>
2021-05-21 22:33:19 -0600

    Added About Us

---
+++ frontend/src/containers/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  { Component } from 'react';
+
+
+
+
+export default class AboutUs extends Component {
+    render(){
+        return(
+            <div>
+                "We are on About Us Page"
+            </div>
+        )
+    }
+}
